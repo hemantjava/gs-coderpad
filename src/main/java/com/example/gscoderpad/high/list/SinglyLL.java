98b5c36d823e82786c89ba9f1a06132318c2c7b4
hemsahu1 <hemant>
2021-03-10 12:58:25 +0530

    Reverse LL.

--- src/main/java/com/example/gscoderpad/high/list/SinglyLL.java
+++ src/main/java/com/example/gscoderpad/high/list/SinglyLL.java
@@ -3,113 +3,117 @@ package com.example.gscoderpad.high.list;
 public class SinglyLL {
     private Node head;
 
-    public void insert(int data){
+    public void insert(int data) {
         Node newNode = new Node(data);
         newNode.setNext(null);
-       // if head is null
+        // if head is null
         if (head == null)
             head = newNode;
-        else
-        {
+        else {
             Node lastNode = head;
-            while (lastNode.getNext() != null){
+            while (lastNode.getNext() != null) {
                 lastNode = lastNode.getNext(); //finding last node of list
             }
             lastNode.setNext(newNode);//setting newNode to last node
         }
     }
-    public void print(){
+
+    public void print() {
         Node currentNode = head;
-        while (currentNode != null){
-            System.out.print(currentNode.getData()+" ");
+        while (currentNode != null) {
+            System.out.print(currentNode.getData() + " ");
             currentNode = currentNode.getNext();
         }
         System.out.println("\n");
 
     }
+    //Display
 
-    public int length(){
+    public String display() {
+        StringBuilder sb = new StringBuilder();
+        if (head == null) {
+            return sb.append("[]").toString();
+        } else {
+
+            Node currentNode = head;
+            sb.append('[');
+            while (currentNode != null) {
+                sb.append(currentNode.getData());
+                currentNode = currentNode.getNext();
+                if (currentNode == null) {
+                    return sb.append(']').toString();
+                }
+                sb.append(',');
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public int length() {
         int count = 0;
         Node currentNode = head;
-        while (currentNode!=null){
+        while (currentNode != null) {
             count++;
             currentNode = currentNode.getNext();
         }
         return count;
     }
 
-    public void insertFirst(int data){
+    public void insertFirst(int data) {
         Node newNode = new Node(data);
         newNode.setNext(head);
         head = newNode;
     }
-    public void insertLast(int data){
-        Node newNode = new Node(data);
-        if(head==null){
-            head = newNode;
-            return;
-        }
-        Node lastNode = head;
-        while (lastNode.getNext()!=null){
-            lastNode = lastNode.getNext();
-        }
-        lastNode.setNext(newNode);
+
+
+    public boolean isEmpty() {
+        return (head == null) ? true : false;
     }
 
-    public boolean isEmpty(){
-       return  (head==null)?true:false;
-    }
-
-    public boolean deleteFirst(){
-        if(head==null){
+    public boolean deleteFirst() {
+        if (head == null) {
             return false;
         }
-        Node currentNode = head;
         head = head.getNext();
-        currentNode.setNext(null);
         return true;
     }
 
-    public Node reverse(){
-        if (head == null){
-            return null;
+    public void reverse() {
+        if (head == null) {
+            return;
         }
         Node currentNode = head;
         Node previous = null;
         Node next = null;
 
-        while (currentNode!=null){
+        while (currentNode != null) {
             next = currentNode.getNext();
             currentNode.setNext(previous);
             previous = currentNode;
             currentNode = next;
         }
-        return previous;
+        head = previous;
+
     }
 
-    public int findData(int searchValue){
-        if (head==null)
+    public int findData(int searchValue) {
+        if (head == null)
             return -1;
         Node currentNode = head;
-        while (currentNode.getNext()!=null){
+        while (currentNode.getNext() != null) {
             if (currentNode.getData() == searchValue)
                 return currentNode.getData();
             currentNode = currentNode.getNext();
         }
-        return  -1;
+        return -1;
     }
 
-    // prints content of double linked list
-    void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.getData() + " ");
-            node = node.getNext();
-        }
-    }
+
 
     public static void main(String[] args) {
         SinglyLL singlyLL = new SinglyLL();
-        System.out.println("IsEmpty:"+singlyLL.isEmpty());
+        System.out.println("IsEmpty:" + singlyLL.isEmpty());
         System.out.println(singlyLL.length());
         singlyLL.insert(1);
         singlyLL.insert(2);
@@ -117,10 +121,9 @@ public class SinglyLL {
         singlyLL.insert(4);
         singlyLL.insert(5);
         singlyLL.insert(6);
-        System.out.println("IsEmpty:"+singlyLL.isEmpty());
+        System.out.println("IsEmpty:" + singlyLL.isEmpty());
         singlyLL.print();
         singlyLL.insertFirst(11);
-        singlyLL.insertLast(12);
         singlyLL.print();
         System.out.println(singlyLL.length());
         singlyLL.print();
@@ -129,8 +132,8 @@ public class SinglyLL {
         System.out.println(singlyLL.findData(21));
         System.out.println(singlyLL.findData(20));
         singlyLL.print();
-        Node node = singlyLL.reverse();
-        singlyLL.printList(node);
+        singlyLL.reverse();
+       System.out.println(singlyLL.display());
     }
 
 
