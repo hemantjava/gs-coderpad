e0d9562e3d3ea5a5d0355ef5dab39f2ed3b4b96f
hemsahu1 <hemant>
2021-03-12 11:45:47 +0530

    NumberInFractionAfterDecimal

--- src/main/java/com/example/gscoderpad/high/NumberInFractionAfterDecimal.java
+++ src/main/java/com/example/gscoderpad/high/NumberInFractionAfterDecimal.java
@@ -7,9 +7,9 @@ public class NumberInFractionAfterDecimal {
         float d = (float) num / (float) den;
         final String value = String.valueOf(d);
         StringBuilder sb = new StringBuilder();
-        final String[] split = value.split("\\.");
+        final String[] split = value.split("\\."); //0.5494 0r 0.33788
         System.out.println(Arrays.toString(split));
-        sb.append(split[0]).append(".(");
+        sb.append(split[0]).append(".(");   //0.(
         char[] chars = split[1].toCharArray();
         if (chars.length>=2){
             if((chars[1]-'0')==4){
@@ -30,9 +30,11 @@ public class NumberInFractionAfterDecimal {
     }
 
     public static void main(String[] args) {
-       System.out.println(fractionRepresentation(1, 3));
-        System.out.println(fractionRepresentation(1, 2));
-       System.out.println(fractionRepresentation(6, 11));
-        System.out.println(fractionRepresentation(60, 3));
+        if (fractionRepresentation(1, 2).equals("0.5") && fractionRepresentation(6, 11).equals("0.(54)")
+                && fractionRepresentation(1, 3).equals("0.(3)")) {
+            System.out.println("All passed");
+        } else {
+            System.out.println("Failed");
+        }
     }
 }
